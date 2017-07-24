2de8bf37e6846d04fda77646350794d15fe53382
Samuel Wellander <samwellander@gmail.com>
2017-07-24 12:22:45 -0700

    Add functionality to Log Out button

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: "AIzaSyDKZESiDnmvfjjRj0jJ8yp0FJy6ZT6dxS0",
@@ -13,13 +15,39 @@ class App extends Component {
       projectId: "auth-c0c75",
       storageBucket: "auth-c0c75.appspot.com",
       messagingSenderId: "935754404808"
-  });
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent() {
+      switch (this.state.loggedIn) {
+        case true:
+          return (
+            <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+            </CardSection>
+          );
+        case false:
+          return <LoginForm />;
+        default:
+          return <Spinner size="large" />;
+      }
+    }
+
   render() {
     return (
       <View>
-        <Header headerText="Authentication Demo" />
-        <LoginForm />
+        <Header headerText="Authentication" />
+        {this.renderContent()}
       </View>
     );
   }
